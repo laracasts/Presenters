06b0025c2671f9fedabc6485f452f6b6b2c154d0
laracasts <jeffrey@laracasts.com>
2013-11-05 06:48:36 -0500

    Just extend Illuminate's collection

--- Presenters/PresenterCollection.php
+++ Presenters/PresenterCollection.php
@@ -1,48 +1,17 @@
 <?php namespace Acme\Presenters;
 
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection;
 
-class PresenterCollection implements \Countable, \ArrayAccess, \IteratorAggregate {
+class PresenterCollection extends Collection {
+    
+    public function __construct($presenter, Collection $collection)
+    {
+        foreach($collection as $key => $resource)
+        {
+            $collection->put($key, new $presenter($resource));
+        }
 
-	protected $collection;
-
-	public function __construct($presenter, Collection $collection)
-	{
-		foreach($collection as $key => $resource)
-		{
-			$collection->put($key, new $presenter($resource));
-		}
-
-		$this->collection = $collection;
-	}
-
-	public function count()
-	{
-		return count($this->collection);
-	}
-
-	public function getIterator()
-	{
-		return $this->collection;
-	}
-
-	public function offsetExists($offset)
-	{
-		return isset($this->collection[$offset]);
-	}
-
-	public function offsetGet($offset)
-	{
-		return isset($this->collection[$offset]) ? $this->collection[$offset] : null;
-	}
-
-	public function offsetSet($offset, $value)
-	{
-		$this->collection[$offset] = $value;
-	}
-
-	public function offsetUnset($offset)
-	{
-		unset($this->collection[$offset]);
-	}
+        $this->items = $collection->toArray();
+    }
+    
 }
